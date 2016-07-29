f3ec273d167f492f0efd4ec77e75b6ce84124fe1
Lei Xu <leix@uber.com>
2016-07-28 22:37:26 -0700

    Initial Commit

---
+++ Map-Kit-Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Map-Kit-Test
+//
+//  Created by Lei Xu on 7/28/16.
+//  Copyright © 2016 Uber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Map-Kit-Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Map-Kit-Test
+//
+//  Created by Lei Xu on 7/28/16.
+//  Copyright © 2016 Uber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
